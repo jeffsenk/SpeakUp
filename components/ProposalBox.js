c59888c8e57a516ea00ab4277bd087c3ad198a66
jeffsenk <jeff.senk@gmail.com>
2017-10-14 19:11:05 +0700

    outline for ProposalBox

--- components/ProposalBox.js
+++ components/ProposalBox.js
@@ -15,7 +15,23 @@ export default class ProposalBox extends Component<{}>{
   render(){
     return(
       <View style={styles.outer}>
-        <Text>{this.props.name}</Text>
+        <Text style={styles.category}>{this.props.name}</Text>
+        <View style={styles.content}>
+          <Text style={styles.title}>Overturn Net Neutrality</Text>
+          <Text style={styles.details}>View Details</Text>
+        </View>
+        <View style={{flex:1,flexDirection:'row',justifyContent:'space-between'}}>
+          <View>
+            <Text style={{color:'teal'}}> UP 31% </Text>
+            <Text style={styles.votes}> UpVoted by John Doe</Text>
+            <Text style={styles.comments}> View 45 comments </Text>
+          </View>
+          <View>
+            <Text style={{color:'salmon'}}> DN 69% </Text>
+            <Text style={styles.votes} >DownVoted by Sheila Grant</Text>
+            <Text style={styles.comments}> View 14 comments </Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -23,9 +39,29 @@ export default class ProposalBox extends Component<{}>{
 }
 
 const styles = StyleSheet.create({
+  comments:{
+    color:'grey'
+  },
+  votes:{
+    fontWeight:'bold'
+  },
   outer:{
-    height:600,
-    borderBottomColor:'black',
+    height:200,
+    borderBottomColor:'lightgray',
     borderBottomWidth:1
+  },
+  category:{
+    marginBottom:5
+  },
+  content:{
+    height:100,
+  },
+  title:{
+    height:50,
+    marginBottom:5,
+    fontSize:25
+  },
+  details:{
+    marginBottom:5
   }
 });
